e0e3534778c4db0d6078d0185e2c5b8e2c949c3c
Wellington Dantas <wellington.dts.silva@gmail.com>
2021-05-27 09:51:09 -0300

    build: create hook pre-commit

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testEnvironment: 'jest-environment-node',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
