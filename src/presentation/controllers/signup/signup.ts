d84c41fc377f15a9503188abf9ea492da2453f20
Wellington Dantas <wellington.silva@eduxe.com.br>
2021-06-06 07:38:11 -0300

    ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -27,13 +27,14 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      const account = this.addAccount.add({
+        name,
+        email,
+        password
+      })
       return {
         statusCode: 200,
-        body: this.addAccount.add({
-          name,
-          email,
-          password
-        })
+        body: account
       }
     } catch (error) {
       return serverError()
